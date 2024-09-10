2ffac7b0e9e2d5234cb7dae3d90f6199afb98f12
httpsclly <marcellybrito44@gmail.com>
2024-09-10 17:24:12 -0300

    fundamentos 02

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+let meta = {
+  value: "Ler um livro todo mês.",
+  checked: true,
+  
+};
 
-let mensagem = "olá mundo"
-mensagem = "nada de olá mundo"
-// posso mudar a variavel pois estou usando a LET
 
-const teste = "olá mundinho."
-// se eu tentar mudar vai dá erro pois a const não muda, ela fica assim constantemente. 
+const criarMeta = () => {}
 
-console.log(mensagem);
-console.log(teste);
+// function criarMet() {}
 
-{
-    const mensagem = "olá mundo dentro de chaves";
-    console.log(mensagem);
-    // tudo que fica dentro de chaves é como se fosse um novo escopo, então mesmo repetindo o nome da variavel em CONST (não muda), ele vai rodar e não dá erro.
-}
+let metas = [
+  meta,
+  {
+    value: "caminhar",
+    checked: false
+  }
+]
 
+console.log(meta.value)
